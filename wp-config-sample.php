73a5a0d83be6bc2d84ec10c20f8fc58f0b17c8bc
Tom <tom@noogen.net>
2015-02-01 02:38:34 -0600

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -62,7 +62,7 @@ if ( $urlParts[0] == 'dev') {
 	define( 'WP_DEBUG', false );
 
   // define root site
-	define('DOMAIN_CURRENT_SITE', 'wpa.gsn2.com');
+	define('DOMAIN_CURRENT_SITE', 'admin.wpa.gsn2.com');
 }
 
 /**
