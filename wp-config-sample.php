f7839dbb5700fc9da16854b32b4106386b56b717
Tom <tom@noogen.net>
2015-02-01 10:10:23 -0600

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -82,52 +82,43 @@ if ( WP_DEBUG == true ) {
 }
 
 /**
- * Salts, for security
- */
-if ( file_exists( dirname( __FILE__ ) . '/../wp-salts.php' ) ) {
-	include dirname( __FILE__ ) . '/../wp-salts.php';
-} else {
-
-	/**#@+
-	* Authentication Unique Keys and Salts.
-	*
-	* Change these to different unique phrases!
-	* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-	* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-	*
-	* @since 2.6.0
-	*/
-	define('AUTH_KEY',         'put your unique phrase here');
-	define('SECURE_AUTH_KEY',  'put your unique phrase here');
-	define('LOGGED_IN_KEY',    'put your unique phrase here');
-	define('NONCE_KEY',        'put your unique phrase here');
-	define('AUTH_SALT',        'put your unique phrase here');
-	define('SECURE_AUTH_SALT', 'put your unique phrase here');
-	define('LOGGED_IN_SALT',   'put your unique phrase here');
-	define('NONCE_SALT',       'put your unique phrase here');
-
-	/**#@-*/
-}
-
-/**
-* Salts, for security
+* custom config if any
 */
-if ( file_exists( dirname( __FILE__ ) . '/../my-db.php' ) ) {
-	include dirname( __FILE__ ) . '/../my-db.php';
+if ( file_exists( dirname( __FILE__ ) . '/../my-config.php' ) ) {
+	include dirname( __FILE__ ) . '/../my-config.php';
 } else {
-
-	// ** MySQL settings - You can get this info from your web host ** //
-	/** The name of the database for WordPress */
-	define('DB_NAME', 'database_name_here');
-
-	/** MySQL database username */
-	define('DB_USER', 'username_here');
-
-	/** MySQL database password */
-	define('DB_PASSWORD', 'password_here');
-
-	/** MySQL hostname */
-	define('DB_HOST', 'localhost');
+	
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'database_name_here');
+/** MySQL database username */
+define('DB_USER', 'username_here');
+/** MySQL database password */
+define('DB_PASSWORD', 'password_here');
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+/**#@-*/
 
 }
 
