46c98c97dc437fc643f37425ed7c3244deda095a
Tom <tom@noogen.net>
2015-02-01 01:35:14 -0600

    Create my-salt.php

---
+++ my-salt.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
