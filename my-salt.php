9eb91602b27efdcc74de55f9ba87c40c09fdfd9c
Tom <tom@noogen.net>
2015-02-01 01:35:46 -0600

    Update my-salt.php

--- my-salt.php
+++ my-salt.php
@@ -1,9 +1,19 @@
 <?php
-define('AUTH_KEY',         '');
-define('SECURE_AUTH_KEY',  '');
-define('LOGGED_IN_KEY',    '');
-define('NONCE_KEY',        '');
-define('AUTH_SALT',        '');
-define('SECURE_AUTH_SALT', '');
-define('LOGGED_IN_SALT',   '');
-define('NONCE_SALT',       '');
+/**#@+
+	* Authentication Unique Keys and Salts.
+	*
+	* Change these to different unique phrases!
+	* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+	* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+	*
+	* @since 2.6.0
+	*/
+	define('AUTH_KEY',         'put your unique phrase here');
+	define('SECURE_AUTH_KEY',  'put your unique phrase here');
+	define('LOGGED_IN_KEY',    'put your unique phrase here');
+	define('NONCE_KEY',        'put your unique phrase here');
+	define('AUTH_SALT',        'put your unique phrase here');
+	define('SECURE_AUTH_SALT', 'put your unique phrase here');
+	define('LOGGED_IN_SALT',   'put your unique phrase here');
+	define('NONCE_SALT',       'put your unique phrase here');
+	/**#@-*/
