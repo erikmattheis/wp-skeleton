840de663c80bbce098f6e655bc6be3cd81d7fc28
Tom <tom@noogen.net>
2015-02-01 09:24:39 -0600

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,11 +16,7 @@
 
 define( 'SUNRISE', 'on' );
 define( 'WP_ALLOW_MULTISITE', true );
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', true );
-define( 'PATH_CURRENT_SITE', '/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
+//MULTISITE_CONFIG
 
 /**
 * Language (leave blank for American English)
