b78c012a8bcfa185cee583028b09ac87f90ca1a0
Tom <tom@noogen.net>
2015-02-26 15:48:48 -0600

    Update functions.js

--- content/themes/gsn-theme-basic/inc/js/dev/functions.js
+++ content/themes/gsn-theme-basic/inc/js/dev/functions.js
@@ -1,27 +1,4 @@
-// Bootstrap specific functions and styling
-jQuery(document).ready(function(){
-
-	// here for each comment reply link of WordPress
-	jQuery( '.comment-reply-link' ).addClass( 'btn btn-sm btn-default' );
-
-	// here for the submit button of the comment reply form
-	jQuery( '#submit, button[type=submit], button, html input[type=button], input[type=reset], input[type=submit]' ).addClass( 'btn btn-default' );
-
-	// Now we'll add some classes for the WordPress default widgets - let's go
-	jQuery( '.widget_rss ul' ).addClass( 'media-list' );
-
-	// Add Bootstrap style for drop-downs
-	jQuery( '.postform' ).addClass( 'form-control' );
-
-	// Add Bootstrap styling for tables
-	jQuery( 'table#wp-calendar' ).addClass( 'table table-striped');
-
-	jQuery( '#submit, .tagcloud, button[type=submit], .comment-reply-link, .widget_rss ul, .postform, table#wp-calendar' ).show( "fast" );
-
-});
-
 // jQuery powered scroll to top
-
 jQuery(document).ready(function(){
 
 	//Check to see if the window is top if not then display button
